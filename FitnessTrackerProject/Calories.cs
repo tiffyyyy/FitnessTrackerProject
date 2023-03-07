fed60671d9534d3186167d1e4abaf65bbb8c7be4
Tiffany Andrade <114040840+tiffyyyy@users.noreply.github.com>
2023-03-07 17:03:53 +0800

    Updated class

--- FitnessTrackerProject/Calories.cs
+++ FitnessTrackerProject/Calories.cs
@@ -9,28 +9,28 @@ using System.Transactions;
 
 namespace FitnessTrackerProject
 {
-    internal class CalculateCalories : Workout
+    internal class Calories : Workout
     {
         public int CaloriesPerMinute { get; set; }
 
 
-        public CalculateCalories(string? type, int numberOfDays, string duration, Intensity intensity) : base(type, numberOfDays, duration, intensity)
+        public Calories(string? type, int numberOfDays, int duration, Intensity intensity) : base(type, numberOfDays, duration, intensity)
         {
             this.CaloriesPerMinute = 0;
-            
+
             switch (type)
             {
                 case "Running":
                     switch (intensity)
                     {
                         case Intensity.Light:
-                            CaloriesPerMinute = 1;
+                            CaloriesPerMinute = 20;
                             break;
                         case Intensity.Moderate:
-                            CaloriesPerMinute = 2;
+                            CaloriesPerMinute = 40;
                             break;
-                        case Intensity.Vigorous: 
-                            CaloriesPerMinute = 3;
+                        case Intensity.Vigorous:
+                            CaloriesPerMinute = 60;
                             break;
                     }
                     break;
@@ -38,6 +38,21 @@ namespace FitnessTrackerProject
 
 
         }
+
+        public int CalculateCalories(int CaloriesPerMinute)
+        {
+            CaloriesPerMinute = this.CaloriesPerMinute * base.Duration;
+            return CaloriesPerMinute;
+        }
+
+        public int BurnedCalories(int CaloriesPerMinute, int CaloriesPerServing, Food FoodCal) 
+        {
+            Food Foodcal = new Food("", 0);
+            base.CaloriesBurned = Foodcal.CaloriesPerServing  - CaloriesPerMinute;
+
+            return base.CaloriesBurned;
+        }
     }
+
 }
 
